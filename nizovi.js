f3e73e3a2141b125360764b44c3f144b688d65b6
Nikola Ratinac <nikola.ratinac@prvagimnazija.edu.rs>
2023-11-13 12:07:07 +0100

    postavka zadatka za nizove

---
+++ nizovi.js
@@ -0,0 +1,19 @@
+function PrikaziUcenike(ucenici){
+    console.log("Prikazujem ucenike")
+    ucenici.map(e => {
+        document.documentElement.innerHTML += e.ime + " " + e.prezime + "<br>"
+    })
+}
+
+function SortirajPrezimena(ucenici) {
+
+}
+
+function SortirajProseke(ucenici){
+
+}
+
+function ObeleziOdlicne(ucenici){
+    
+}
+
