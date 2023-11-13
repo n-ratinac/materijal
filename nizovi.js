d9242c8b29784c953209d2750625f045a41c2454
Nikola Ratinac <nikola.ratinac@prvagimnazija.edu.rs>
2023-11-13 12:26:49 +0100

    sort po prezimenu resen

--- nizovi.js
+++ nizovi.js
@@ -5,8 +5,31 @@ function PrikaziUcenike(ucenici){
     })
 }
 
-function SortirajPrezimena(ucenici) {
+function SortPo(prop) {    
+    return function(a, b) {    
+        if (a[prop] > b[prop]) {    
+            return 1;    
+        } else if (a[prop] < b[prop]) {    
+            return -1;    
+        }    
+        return 0;    
+    }    
+}  
 
+function SortirajPrezimena(ucenici) {
+    
+    // laksi nacin
+    // ucenici.sort(SortPo("prezime"))
+    // console.log(ucenici);
+
+    for (i = 0; i < ucenici.length - 1; i++)
+        for (j = i+1; j < ucenici.length; j++)
+            if(ucenici[i]['prezime'] < ucenici[j]['prezime'])
+            {
+                tmp = ucenici[i];
+                ucenici[i] = ucenici[j];
+                ucenici[j] = tmp;
+            }
 }
 
 function SortirajProseke(ucenici){
@@ -14,6 +37,6 @@ function SortirajProseke(ucenici){
 }
 
 function ObeleziOdlicne(ucenici){
-    
+
 }
 
