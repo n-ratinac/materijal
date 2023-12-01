4921d8cb8b5dc4c32f9ab373bdaebf1bb39ce82a
Nikola Ratinac <nikola.ratinac@prvagimnazija.edu.rs>
2023-12-01 18:04:19 +0100

    update data

--- stateful-app/data.js
+++ stateful-app/data.js
@@ -1,7 +1,7 @@
 items = [
     {"title": "Wash dishes", "status": "todo"},
-    {"title": "Wash dishes", "status": "todo"},
-    {"title": "Wash dishes", "status": "todo"},
-    {"title": "Wash dishes", "status": "todo"},
-    {"title": "Wash dishes", "status": "todo"},
+    {"title": "Vacuum", "status": "todo"},
+    {"title": "Take the dog for a walk", "status": "todo"},
+    {"title": "Cook diner", "status": "todo"},
+    {"title": "Relax", "status": "todo"},
 ]
